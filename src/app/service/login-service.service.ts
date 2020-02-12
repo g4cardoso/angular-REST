56fb591899353b2160ae551e78015c730227baf0
gabiel cardoso <gacardoso247@gmail.com>
2020-02-12 20:18:37 -0300

    Projeto criado

---
+++ src/app/service/login-service.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AppConstants } from '../app-constants';
+import { error } from '@angular/compiler/src/util';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  login(usuario) {
+
+
+     return this.http.post(AppConstants.baseLogin, JSON.stringify(usuario)).subscribe(data =>  {
+
+
+    var token = JSON.parse(JSON.stringify(data)).Authorization.split(' ')[1];
+
+    localStorage.setItem("token", token);
+
+    },
+      error => {
+        console.error("erro ao fazer login");
+        alert('Acesso negado!')
+      }
+    );
+  }
+
+
+}
